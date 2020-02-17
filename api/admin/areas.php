0a4be9b42f4e9bfb38fab68b07586995ad24d76c
Wildragonblack <https://honeymustard.mx:2096/>
2020-02-16 19:02:27 -0500

    [ADD] Enviar areas desde API

--- api/admin/areas.php
+++ api/admin/areas.php
@@ -1,13 +1,14 @@
 <?php 
 
 require_once('../access.php');
-$mysqli = new mysqli("localhost","root","","sicoa");  // conecta mediante mysqli
+$mysqli = new mysqli("localhost","root","","sicoa");
+$mysqli->query("SET NAMES 'utf8'");
 // <--- Obtiene todas las direcciones y las ordena segun su Id
-$query = "SELECT Id_direcciones, nombre_direccion FROM T_DIRECCIONES ORDER BY Id_direcciones";
+$query = "SELECT Id_direcciones, nombre_direccion FROM t_direcciones ORDER BY Id_direcciones";
 $resultado=$mysqli->query($query);
 $respuesta = array();
 while($row = $resultado->fetch_assoc()) {
-	$query2 = "SELECT nombre_coordinacion FROM T_COORDINACIONES WHERE id_direcciones = {$row['Id_direcciones']}"; 
+	$query2 = "SELECT nombre_coordinacion FROM t_coordinaciones WHERE Id_direcciones = {$row['Id_direcciones']}"; 
 	$cont = 0;
     $resultado2=$mysqli->query($query2);
     while($row2 = $resultado2->fetch_assoc()) {
